617dfa18dcd3204c926875ee7b2ce2da33235372
Endre Bakken Stovner <endrebak85@gmail.com>
2019-12-10 10:11:11 +0100

    Update gwas_catalog

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # pyranges_db
 
-PyRanges add-on package to fetch annotations from UCSC, gencode and ensembl.
+PyRanges add-on package to fetch annotations from UCSC, gencode, ensembl and the gwas_catalog
 
 [Docs](https://biocore-ntnu.github.io/pyranges/fetching-gene-tracks-as-pyranges.html)
--- pyranges_db/gwas_catalog.py
+++ pyranges_db/gwas_catalog.py
@@ -3,15 +3,24 @@ import pandas as pd
 import pyranges as pr
 
 
-def gwas_catalog():
+def gwas_catalog(nrows=None):
 
     f = "https://www.ebi.ac.uk/gwas/api/search/downloads/alternative"
-    df = pd.read_csv(f, sep="\t", low_memory=False)
+    # f = "/Users/endrebakkenstovner/alternative"
+
+    if nrows is None:
+        df = pd.read_csv(f, sep="\t", low_memory=False)
+    else:
+        import subprocess
+        result = subprocess.check_output("curl -s {} | head -{}".format(f, str(nrows)), shell=True).decode()
+        from io import StringIO
+        df = pd.read_csv(StringIO(result), sep="\t", low_memory=False)
 
     df = df.rename(columns={"CHR_ID": "Chromosome", "CHR_POS": "Start"})
 
-    bad_locs = df.Start.isnull() | df.Chromosome.isnull() | df.Start.str.contains("x") | df.Chromosome.str.contains("x")
+    bad_locs = df.Start.isnull() | df.Chromosome.isnull() | df.Start.astype(str).str.contains("x") | df.Chromosome.astype(str).str.contains("x")
     df = df[~bad_locs]
+    df = df.astype({"Chromosome": str, "Start": str})
 
     # split multiple starts/chromosomes into multiple rows
     new_starts = pd.DataFrame(df.Start.str.split(';', expand=True)).stack().reset_index(level=1, drop=True)
